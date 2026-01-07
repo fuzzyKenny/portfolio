a5f669be705c17bdbcb2d9659ee81b84ae643926
fuzzyKenny <kritiraj.tech@gmail.com>
2026-01-07 21:12:14 +0530

    add color-scheme switch

---
+++ app/components/themeToggle.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { motion } from "motion/react";
+import { Sun, Moon } from "lucide-react";
+
+import { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
+
+export default function ThemeToggle() {
+  const { theme, setTheme, resolvedTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
+  if (!mounted) return null;
+
+  const current = theme === "system" ? resolvedTheme : theme;
+
+  return (
+    <motion.button
+      whileTap={{
+        rotate: 180,
+      }}
+      className="p-3 transition-colors text-zinc-500"
+      onClick={() => setTheme(current === "dark" ? "light" : "dark")}
+    >
+      {theme === "dark" ? <Sun /> : <Moon />}
+    </motion.button>
+  );
+}
