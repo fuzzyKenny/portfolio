dc68ccb9b56a1728d9ce85bcf90cfd07dda6188b
fuzzyKenny <kritiraj.tech@gmail.com>
2026-01-07 23:12:45 +0530

    add profile component

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ThemeToggle from "./components/themeToggle";
+import Profile from "./components/profile";
 
 const Home = () => {
   return (
     <>
-      <main className="min-h-screen transition-colors duration-300 p-4 grid-bg animate-grid-lines -z-10">
-        <div className="max-w-3xl mx-auto flex justify-end">
-          <ThemeToggle />
-        </div>
-        <section className="max-w-3xl mx-auto">
-          <p>Lorem</p>
-        </section>
+      <div className="grid-bg animate-grid-lines -z-10"></div>
+      <div className="flex justify-end max-w-3xl mx-auto mt-24">
+        <ThemeToggle />
+      </div>
+      <main className="max-w-4xl mx-auto p-4">
+        <Profile />
       </main>
     </>
   );
