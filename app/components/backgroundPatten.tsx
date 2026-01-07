46d38e26163267af946cf5c1145461b2867c58e3
fuzzyKenny <kritiraj.tech@gmail.com>
2026-01-08 00:51:04 +0530

    fix backgronud pattern glitches

---
+++ app/components/backgroundPatten.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function BgPattern() {
+  return (
+    <>
+      <div className="grid-bg min-h-screen min-w-screen -z-10"></div>
+      <div className="grid-bg-inv min-h-screen min-w-screen -z-10"></div>
+    </>
+  );
+}
