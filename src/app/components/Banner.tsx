1a07d10e411323f2c54699c018d76a14e50c130e
fuzzyKenny <kritiraj.tech@gmail.com>
2026-01-18 23:44:04 +0530

    Something

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -16,7 +16,7 @@ export default function Banner() {
           alt="Profile Image"
           width={300}
           height={300}
-          className="h-25 w-25 sm:h-40 sm:w-40 md:h-45 md:w-45 rounded-full border-white/0 border absolute -bottom-25 left-10"
+          className="h-25 w-25 sm:h-40 sm:w-40 md:h-45 md:w-45 rounded-full absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 md:translate-y-8"
         />
       </div>
     </>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,8 @@ import Banner from "./components/Banner";
 export default function Home() {
   return (
     <>
-      <div className="h-screen max-w-full border-white/20 border mx-auto md:max-w-200 md:p-4">
+      <div className="h-screen max-w-full mx-auto md:max-w-200 md:p-4">
         <Banner />
-        <p className="mt-30 text-2xl">Hi There, I am Kritiraj, aka Kenny</p>
       </div>
     </>
   );
