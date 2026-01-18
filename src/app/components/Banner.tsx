6120a6d57803a14ad015c61be9cce1a5d25f8e91
fuzzyKenny <kritiraj.tech@gmail.com>
2026-01-18 23:26:36 +0530

    Too tired

---
+++ src/app/components/Banner.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+export default function Banner() {
+  return (
+    <>
+      <div className="relative">
+        <Image
+          src="/XBanner.jpeg"
+          alt="Banner Image"
+          width={1000}
+          height={1000}
+          className="rounded-b-2xl md:rounded-4xl"
+        />
+        <Image
+          src="/Xpfp.jpg"
+          alt="Profile Image"
+          width={300}
+          height={300}
+          className="h-25 w-25 sm:h-40 sm:w-40 md:h-45 md:w-45 rounded-full border-white/0 border absolute -bottom-25 left-10"
+        />
+      </div>
+    </>
+  );
+}
